9434870ce4dfc3184de185b662c2717c20807d0f
ejarama <elijllo10@gmail.com>
2025-04-06 23:16:30 -0500

    Se agrega clase factory

--- wSimuladorCarrera/AutoFactory.cs
+++ wSimuladorCarrera/AutoFactory.cs
@@ -8,16 +8,16 @@ namespace wSimuladorCarrera
 {
     public static class AutoFactory
     {
-        public static Auto CrearAuto(string nombre, string tipo,int distancia)
+        public static Auto CrearAuto(string nombre, string tipo)
         {
             switch (tipo)
             {
                 case "AutoDeportivo":
-                    return new AutoTodoterreno(nombre, tipo, distancia);
+                    return new AutoTodoterreno(nombre, tipo);
                 case "AutoTodoterreno":
-                    return new AutoTodoterreno(nombre, tipo, distancia);
+                    return new AutoTodoterreno(nombre, tipo);
                 case "AutoHibrido":
-                    return new AutoTodoterreno(nombre, tipo, distancia);
+                    return new AutoTodoterreno(nombre, tipo);
                 default:
                     throw new ApplicationException("El tipo de auto no está definido");
             }
